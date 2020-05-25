67d96ec9e45acaac22ce3bca51e17c1399482250
AssafMeirom <50071206+AssafMeirom@users.noreply.github.com>
2020-05-25 15:10:11 +0300

    Add files via upload

---
+++ MasterCommands3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+cd /tmp
+sudo curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
+sudo chmod u+x install-helm.sh
+./install-helm.sh
+sudo helm init --upgrade
+sudo kubectl create serviceaccount --namespace kube-system tiller
+sudo kubectl create clusterrolebinding tiller-cluster-rule \
+   --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+sudo kubectl patch deploy --namespace kube-system tiller-deploy \
+   -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+sudo kubectl create -f  /home/ubuntu/mariadb-hostpath.yaml
+sudo kubectl create -f  /home/ubuntu/wordpress-hostpath.yaml
+sudo kubectl create -f /home/ubuntu/wordpress-mariadb-pvc.yaml 
+sudo kubectl create -f /home/ubuntu/wordpress-pvc.yaml
+sudo helm install --name wordpress \
+  --set wordpressUsername=admin,wordpressPassword=adminpassword,mariadb.mariadbRootPassword=secretpassword,persistence.existingClaim=wordpress-wordpress,allowEmptyPassword=false \
+    stable/wordpress
