0faa4eb1d1a2d00c5f047daebbeb2c78c5fdbc17
AssafMeirom <50071206+AssafMeirom@users.noreply.github.com>
2020-05-25 16:03:01 +0300

    Update MasterCommands4.sh

--- MasterCommands4.sh
+++ MasterCommands4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo kubectl create -f  /home/ubuntu/mariadb-hostpath.yaml
-sudo kubectl create -f  /home/ubuntu/wordpress-hostpath.yaml
-sudo kubectl create -f /home/ubuntu/wordpress-mariadb-pvc.yaml 
-sudo kubectl create -f /home/ubuntu/wordpress-pvc.yaml
+sudo kubectl create -f  /home/ubuntu/TestingLinux/mariadb-hostpath.yaml
+sudo kubectl create -f  /home/ubuntu/TestingLinux/wordpress-hostpath.yaml
+sudo kubectl create -f /home/ubuntu/TestingLinux/wordpress-mariadb-pvc.yaml 
+sudo kubectl create -f /home/ubuntu/TestingLinux/wordpress-pvc.yaml
 sudo helm install --name wordpress \
   --set wordpressUsername=admin,wordpressPassword=adminpassword,mariadb.mariadbRootPassword=secretpassword,persistence.existingClaim=wordpress-wordpress,allowEmptyPassword=false \
     stable/wordpress
