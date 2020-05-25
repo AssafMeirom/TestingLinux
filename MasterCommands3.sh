945b3c9da6a352e4a627236c73ef0158ccdf40f7
AssafMeirom <50071206+AssafMeirom@users.noreply.github.com>
2020-05-25 15:51:46 +0300

    Update MasterCommands3.sh

--- MasterCommands3.sh
+++ MasterCommands3.sh
@@ -10,10 +10,3 @@ sudo kubectl create clusterrolebinding tiller-cluster-rule \
    --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 sudo kubectl patch deploy --namespace kube-system tiller-deploy \
    -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-sudo kubectl create -f  /home/ubuntu/TestingLinux/mariadb-hostpath.yaml
-sudo kubectl create -f  /home/ubuntu/TestingLinux/wordpress-hostpath.yaml
-sudo kubectl create -f /home/ubuntu/TestingLinux/wordpress-mariadb-pvc.yaml 
-sudo kubectl create -f /home/ubuntu/TestingLinux/wordpress-pvc.yaml
-sudo helm install --name wordpress \
-  --set wordpressUsername=admin,wordpressPassword=adminpassword,mariadb.mariadbRootPassword=secretpassword,persistence.existingClaim=wordpress-wordpress,allowEmptyPassword=false \
-    stable/wordpress
